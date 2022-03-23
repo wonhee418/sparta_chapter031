f40ae5d3264e47f62a722e1ed0519f370ae22eee
wonhee418 <cheeky4@naver.com>
2022-03-23 14:01:03 +0900

    Create BoardService.java

---
+++ src/main/java/com/sparta/chapter031/service/BoardService.java
@@ -0,0 +1,25 @@
+package com.sparta.chapter031.service;
+
+import com.sparta.chapter031.domain.Board;
+import com.sparta.chapter031.domain.BoardRepository;
+import com.sparta.chapter031.domain.BoardRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long update(Long id, BoardRequestDto requestDto) {
+        Board board = boardRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        board.update(requestDto);
+        return board.getId();
+    }
+}
